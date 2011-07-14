d62e92a449165321441c36de468e99e6c970ed48
Richard West <rwest@mit.edu>
2011-07-14 14:46:30 -0400

    Changed some media urls for socialauth

--- rmgweb/media/socialauth/js/openid-jquery.js
+++ rmgweb/media/socialauth/js/openid-jquery.js
@@ -85,7 +85,7 @@ var openid = {
 	cookie_name: 'openid_provider',
 	cookie_path: '/',
 	
-	img_path: '/site_media/images/',
+	img_path: '/media/socialauth/images/',
 	
 	input_id: null,
 	provider_url: null,
